3912991533876eb2d5c05c492978c7ec0d1e90f5
Prahlad Kumar <neowakesup7@gmail.com>
2018-02-11 20:52:35 +0530

    Add files via upload

---
+++ Buzzer_Arduino_code/Buzzer_Arduino_code.ino
@@ -0,0 +1,69 @@
+ 
+
+int buzzerPin = 10;                                                          
+int smokeinPin = A1;
+int burninPin = 7;
+float tempinPin = A0;
+void setup() {                                                               
+  Serial.begin(9600); 
+  pinMode(buzzerPin, OUTPUT);                               
+  pinMode(tempinPin, INPUT);
+  pinMode(smokeinPin, INPUT);
+  pinMode(burninPin, INPUT);
+
+  beep(50);                                                 
+
+  beep(50);                                                                         
+
+  delay(1000);                                              
+
+}
+
+void loop() {                                               
+
+  float temp;
+  int smoke;
+  int flame;
+
+  temp = analogRead(tempinPin);
+  Serial.print("\nTemperature reading :");
+  Serial.print(temp);
+  smoke = analogRead(smokeinPin);
+  Serial.print("\nSmoke state :");
+  Serial.print(smoke);
+  flame = digitalRead(burninPin);
+  Serial.print("\nFlame state :");
+  Serial.print(flame);
+  if(temp>50)
+  {
+  beep(50);                                                                         
+  
+  delay(1000);  
+  }
+  if(smoke>500)
+  {
+  beep(50);                                                                          
+  
+  delay(50);
+  analogWrite(tempinPin, LOW);                                                                     
+  }
+  if(flame == 0)
+  {
+  analogWrite(tempinPin, LOW);
+  analogWrite(smokeinPin, LOW);  
+  beep(50);                                                                       
+  
+  delay(20);                                                                     
+  }
+}
+
+void beep(unsigned char delayms) {                       // Created a function for beep
+
+  analogWrite(buzzerPin, 20);                                      // This will set pin 11 to high
+
+  delay(delayms);                                                             // Giving a delay
+
+  analogWrite(buzzerPin ,0);                                        // This will set pin 11 to LOW
+
+  delay(delayms);                                                             // Giving a delay
+}
---
+++ Fire_Alert_NodeMCU_code/Fire_Alert_NodeMCU_code.ino
@@ -0,0 +1,141 @@
+#include<ESP8266WiFi.h>
+
+String apiKey ="paste_your_api_key_here";
+
+char thingSpeakAddress[] = "api.thingspeak.com";
+String thingtweetAPIKey = "paste_your_thingtweetapi_key_here"; 
+const char* ssid = "Your_ssid_here";
+const char* password = "Your_ssid_password";
+String twitmsg = "This is twitthings msg";
+const char* server ="api.thingspeak.com";
+
+// Variable Setup
+long lastConnectionTime = 0; 
+boolean lastConnected = false;
+int failedCounter = 0;
+float temp = 0;
+int smoke;
+int flame;
+int analog = 0;
+int digital = 1;
+WiFiClient client;
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() !=WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
+    Serial.println("");
+    Serial.println("WiFi connected");
+
+}
+
+void loop() {
+ analog = analogRead(A0);
+ float temp =analog * 0.322265625;
+ digital = (digitalRead(D0)==LOW);
+ int smoke =digital;
+ digital = (digitalRead(D1)==LOW);
+ int flame =digital;
+
+ if (client.connect(server,80)){//"184.106.153.149"
+  String postStr = apiKey;
+         postStr +="&field1=";
+         postStr += String(temp);
+         postStr +="&field2=";
+         postStr += String(smoke);
+         postStr +="&field3=";
+         postStr += String(flame);
+         postStr +="\r\n\r\n";
+
+
+  client.print("POST /update HTTP/1.1\n");
+  client.print("Host: api.thingspeak.com\n");
+  client.print("Connection: close\n");
+  client.print("X-THINGSPEAKAPIKEY: "+apiKey+"\n");
+  client.print("Content-Type: application/x-www-form-urlencoded\n");
+  client.print("Content-Length: ");
+  client.print(postStr.length());
+  client.print("\n\n");
+  client.print(postStr);
+  
+  Serial.print("Temperature: ");
+  Serial.print(temp);
+  Serial.print("\nSmoke status: ");
+  Serial.print(smoke);
+  Serial.print("\nFlame status: ");
+  Serial.print(flame);
+  Serial.print("\nsending data to thingspeak");
+  
+ }
+ client.stop();
+ Serial.println("\nWaiting...");
+ if(smoke == 1){
+  updateTwitterStatus("Smoke Detected"); 
+ }
+ if(temp>50){ 
+  updateTwitterStatus("Temperature rising"); 
+ }
+ if(flame == 1){
+  
+  updateTwitterStatus("Fire Detected"); 
+ }
+ //thinspeak needs minimum 15 sec delay between updates
+ delay(500);
+}
+void updateTwitterStatus(String tsData)
+{
+  if (client.connect(thingSpeakAddress, 80))
+  { 
+    // Create HTTP POST Data
+    tsData = "api_key="+thingtweetAPIKey+"&status="+tsData;
+            
+    client.print("POST /apps/thingtweet/1/statuses/update HTTP/1.1\n");
+    client.print("Host: api.thingspeak.com\n");
+    client.print("Connection: close\n");
+    client.print("Content-Type: application/x-www-form-urlencoded\n");
+    client.print("Content-Length: ");
+    client.print(tsData.length());
+    client.print("\n\n");
+
+    client.print(tsData);
+    
+    lastConnectionTime = millis();
+    
+    if (client.connected())
+    {
+      Serial.println("Connecting to ThingSpeak...");
+      Serial.println();
+      
+      failedCounter = 0;
+    }
+    else
+    {
+      failedCounter++;
+  
+      Serial.println("Connection to ThingSpeak failed ("+String(failedCounter, DEC)+")");   
+      Serial.println();
+    }
+    
+  }
+  else
+  {
+    failedCounter++;
+    
+    Serial.println("Connection to ThingSpeak Failed ("+String(failedCounter, DEC)+")");   
+    Serial.println();
+    
+    lastConnectionTime = millis(); 
+  }
+}
+
